aa0a3c72b5ebf519ef823abe836a0a46b7d2b4f0
Luke Gartland <lukegartland@gmail.com>
2020-12-23 14:49:29 -1000

    Updates

---
+++ precache-manifest.c860125ae3313fa2ecfbb99ebcfdaa17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2531b735754fc3f94040",
+    "url": "/lightsout/static/css/main.23e91043.chunk.css"
+  },
+  {
+    "revision": "2531b735754fc3f94040",
+    "url": "/lightsout/static/js/main.2531b735.chunk.js"
+  },
+  {
+    "revision": "a989bfac1d920b4f2190",
+    "url": "/lightsout/static/js/1.a989bfac.chunk.js"
+  },
+  {
+    "revision": "5140b50a75f4729ab6d5",
+    "url": "/lightsout/static/js/runtime~main.5140b50a.js"
+  },
+  {
+    "revision": "958e356b8bdb674267fa830f6c308841",
+    "url": "/lightsout/index.html"
+  }
+];
